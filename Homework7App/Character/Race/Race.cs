144e31d607609f0321506bdc7d4f5d15954f2839
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:03 +0200

    Create Race.cs

---
+++ Homework7App/Character/Race/Race.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Race
+{
+    public abstract class Race
+    {
+        public string CharacterRace;
+        public int RaceHealth;
+        public int RaceMana;
+        public int RaceStreghth;
+        public int RaceAgilty;
+        public int RaceIntelect;
+    }
+}
