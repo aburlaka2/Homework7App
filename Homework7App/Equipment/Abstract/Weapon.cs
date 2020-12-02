02855e0dddc7b77042a0acf3129bee227e76294b
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:35 +0200

    Create Weapon.cs

---
+++ Homework7App/Equipment/Abstract/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Homework7App.Equipment.Abstract;
+
+namespace Homework7App.Equipment.Abstract
+{
+    class Weapon:Item, IWearable
+    {
+        public int slot { get; set; }
+        public string slotName { get; set; }
+        public void WearItem()
+        {
+
+        }
+    }
+}
