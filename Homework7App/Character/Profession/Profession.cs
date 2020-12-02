9c97d60c030fa1aa3b5c19e07d4198ff8306e05c
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:25:22 +0200

    Create Profession.cs

---
+++ Homework7App/Character/Profession/Profession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Profession
+{
+    public abstract class Profession
+    {
+        public string CharacterProfession;
+        public int ProfessionHealth;
+        public int ProfessionMana;
+        public int ProfessionStreghth;
+        public int ProfessionAgilty;
+        public int ProfessionIntelect;
+    }
+}
