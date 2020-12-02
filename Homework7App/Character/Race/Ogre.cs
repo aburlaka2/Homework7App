36d4931b818af24be697a784500e7054eb0e2db1
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:25:57 +0200

    Create Ogre.cs

---
+++ Homework7App/Character/Race/Ogre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Race
+{
+    class Ogre:Race, INotPlayble
+    {
+        public Ogre()
+        {
+            CharacterRace = "Ogre";
+            RaceHealth = 10;
+            RaceMana = 6;
+            RaceStreghth = 10;
+            RaceAgilty = 5;
+            RaceIntelect = 3;
+        }
+    }
+}
