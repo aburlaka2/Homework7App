d44830f2718f19fde8fc1032023ef6f32b7780c8
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:25:48 +0200

    Create Human.cs

---
+++ Homework7App/Character/Race/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Race
+{
+    class Human: Race, IPlayble
+    {
+        public Human()
+        {
+            CharacterRace = "Human";
+            RaceHealth = 15;
+            RaceMana = 8;
+            RaceStreghth = 12;
+            RaceAgilty = 6;
+            RaceIntelect = 4;
+        }
+    }
+}
