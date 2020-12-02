a2fdd3961173eea364aa6a313f4c236a42dc165e
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:29 +0200

    Create IWearable.cs

---
+++ Homework7App/Equipment/Abstract/IWearable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Equipment.Abstract
+{
+    interface IWearable
+    {
+        public int slot { get; set; }
+        public string slotName { get; set; }
+        public void WearItem();
+    }
+}
