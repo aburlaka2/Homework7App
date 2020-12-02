cddb0bfcaa007568241861f44fd89d7e60482b10
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:06 +0200

    Create Troll.cs

---
+++ Homework7App/Character/Race/Troll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Race
+{
+    class Troll:Race,INotPlayble
+    {
+        public Troll()
+        {
+        CharacterRace = "Troll";
+        RaceHealth = 15;
+        RaceMana = 8;
+        RaceStreghth = 12;
+        RaceAgilty = 6;
+        RaceIntelect = 4;
+        }
+    }
+}
