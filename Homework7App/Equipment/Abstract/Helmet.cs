7e1229a918d5a323bf0d54a88cb25e5f0166099a
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:22 +0200

    Create Helmet.cs

---
+++ Homework7App/Equipment/Abstract/Helmet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Homework7App.Equipment.Abstract;
+
+namespace Homework7App.Equipment
+{
+    public abstract class Helmet:Item, IWearable
+    {
+        public int slot { get; set; }
+        public string slotName { get; set; }
+        public void WearItem()
+        {
+
+        }
+    }
+}
