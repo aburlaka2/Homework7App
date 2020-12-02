7559f4a26e291f9bf7038a3101389ca1b37ff63b
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:25 +0200

    Create Item.cs

---
+++ Homework7App/Equipment/Abstract/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Equipment
+{
+    public abstract class Item
+    {
+        public string name;
+        public int weight;
+        public int price;
+        public int rarity;
+    }
+}
