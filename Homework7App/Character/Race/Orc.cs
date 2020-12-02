7d24d2f13523d60ae193cc8ebd258f69f1860f85
aburlaka2 <aburlaka1@gmail.com>
2020-12-02 17:26:00 +0200

    Create Orc.cs

---
+++ Homework7App/Character/Race/Orc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Character.Race
+{
+    class Orc:Race,INotPlayble
+    {
+        public Orc()
+        {
+            CharacterRace = "Orc";
+            RaceHealth = 8;
+            RaceMana = 4;
+            RaceStreghth = 8;
+            RaceAgilty = 4;
+            RaceIntelect = 2;
+        }
+    }
+}
