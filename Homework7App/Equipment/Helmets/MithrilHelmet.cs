bdb7cd77f2b4aefe0e9aec1d47440fb3cab66f4e
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:46:46 +0200

    Update MithrilHelmet.cs

--- Homework7App/Equipment/Helmets/MithrilHelmet.cs
+++ Homework7App/Equipment/Helmets/MithrilHelmet.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Helmets
 {
-    class MithrilHelmet
+    class MithrilHelmet:Helmet
     {
+        public int defence { get; set; }
+        public MithrilHelmet()
+        {
+            name = "Mithril Helmet";
+            weight = 6;
+            price = 80;
+            rarity = 8;
+            defence = 6;
+        }
     }
 }
