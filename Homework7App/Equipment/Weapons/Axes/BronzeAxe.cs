25e66eb5c39acdbc00fda9b4c53691c358465c19
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:47:46 +0200

    Update BronzeAxe.cs

--- Homework7App/Equipment/Weapons/Axes/BronzeAxe.cs
+++ Homework7App/Equipment/Weapons/Axes/BronzeAxe.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Homework7App.Equipment.Abstract;
 
 namespace Homework7App.Equipment.Weapons.Axes
 {
-    class BronzeAxe
+    class BronzeAxe:Axe
     {
+
     }
 }
