59f9f48eed1e4ddf95347da0322982099811b428
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:43 +0200

    Update Demon.cs

--- Homework7App/Character/Race/Demon.cs
+++ Homework7App/Character/Race/Demon.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Character.Race
 {
-    class Demon:Race,INotPlayble
+    class Demon:Race, INotPlayble
     {
+        public Demon()
+        {
+            CharacterRace = "Demon";
+            RaceHealth = 12;
+            RaceMana = 15;
+            RaceStreghth = 10;
+            RaceAgilty = 8;
+            RaceIntelect = 8;
+        }
     }
 }
