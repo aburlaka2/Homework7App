74249904bd1a4839777da7c191ea883dea38de5d
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:45:42 +0200

    Create MithrilArmor.cs

---
+++ Homework7App/Equipment/Armors/MithrilArmor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Equipment.Armors
+{
+    class MithrilArmor:Armor
+    {
+        public int defence { get; set; }
+        public MithrilArmor()
+        {
+            name = "Mithril Armor";
+            weight = 12;
+            price = 200;
+            rarity = 9;
+            defence = 9;
+        }
+    }
+}
