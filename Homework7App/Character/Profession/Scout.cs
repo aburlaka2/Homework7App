814d726ff9fd75f6f69507007e360b3390f361f1
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:32 +0200

    Update Scout.cs

--- Homework7App/Character/Profession/Scout.cs
+++ Homework7App/Character/Profession/Scout.cs
@@ -6,5 +6,15 @@ namespace Homework7App.Character.Profession
 {
     class Scout:Profession
     {
+        public Scout()
+        {
+            CharacterProfession = "Scout";
+            ProfessionHealth = 3;
+            ProfessionMana = 1;
+            ProfessionStreghth = 4;
+            ProfessionAgilty = 6;
+            ProfessionIntelect = 2;
+        }
     }
 }
+
