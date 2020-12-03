5a5751796a2255f12483a9e6155d8d4c8e6ddc27
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:36 +0200

    Update Thif.cs

--- Homework7App/Character/Profession/Thif.cs
+++ Homework7App/Character/Profession/Thif.cs
@@ -6,5 +6,15 @@ namespace Homework7App.Character.Profession
 {
     class Thif:Profession
     {
+        public Thif()
+        {
+            CharacterProfession = "Thif";
+            ProfessionHealth = 2;
+            ProfessionMana = 1;
+            ProfessionStreghth = 4;
+            ProfessionAgilty = 5;
+            ProfessionIntelect = 4;
+        }
     }
 }
+
