077b19144894c4ab7ea9ce2bc96696ddbca207c2
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:56 +0200

    Update Elf.cs

--- Homework7App/Character/Race/Elf.cs
+++ Homework7App/Character/Race/Elf.cs
@@ -6,5 +6,14 @@ namespace Homework7App.Character.Race
 {
     class Elf:Race,IPlayble
     {
+        public Elf()
+        {
+            CharacterRace = "Elf";
+            RaceHealth = 4;
+            RaceMana = 8;
+            RaceStreghth = 4;
+            RaceAgilty = 4;
+            RaceIntelect = 4;
+        }
     }
 }
