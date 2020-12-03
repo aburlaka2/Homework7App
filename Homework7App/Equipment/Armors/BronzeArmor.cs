676c5fffcf116a043661738c8c109bb0a32f55c4
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:45:27 +0200

    Update BronzeArmor.cs

--- Homework7App/Equipment/Armors/BronzeArmor.cs
+++ Homework7App/Equipment/Armors/BronzeArmor.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Armors
 {
-    class BronzeArmor
+    class BronzeArmor:Armor
     {
+        public int defence { get; set; }
+        public BronzeArmor()
+        {
+            name = "Bronze Armor";
+            weight = 10;
+            price = 50;
+            rarity = 4;
+            defence = 4;
+        }
     }
 }
