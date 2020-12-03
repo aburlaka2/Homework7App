16697c05a7411cf7fb3b10c8ab8cd48c5681e366
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:47:05 +0200

    Update IronShield.cs

--- Homework7App/Equipment/Shields/IronShield.cs
+++ Homework7App/Equipment/Shields/IronShield.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Shields
 {
-    class IronShield
+    class IronShield:Shield
     {
+        public int defence { get; set; }
+        public IronShield()
+        {
+            name = "Iron Shield";
+            weight = 9;
+            price = 40;
+            rarity = 5;
+            defence = 5;
+        }
     }
 }
