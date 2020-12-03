5a99c74a576eea0da7ada17e57d9a45f544753af
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:24 +0200

    Update Mage.cs

--- Homework7App/Character/Profession/Mage.cs
+++ Homework7App/Character/Profession/Mage.cs
@@ -6,5 +6,14 @@ namespace Homework7App.Character.Profession
 {
     class Mage:Profession
     {
+        public Mage()
+        {
+            CharacterProfession = "Mage";
+            ProfessionHealth = 3;
+            ProfessionMana = 5;
+            ProfessionStreghth = 1;
+            ProfessionAgilty = 1;
+            ProfessionIntelect = 5;
+        }
     }
 }
