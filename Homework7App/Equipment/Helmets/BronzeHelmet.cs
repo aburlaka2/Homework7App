95712f4de63e1fd2bd9f739d3f140c7bd411d2e5
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:46:13 +0200

    Update BronzeHelmet.cs

--- Homework7App/Equipment/Helmets/BronzeHelmet.cs
+++ Homework7App/Equipment/Helmets/BronzeHelmet.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Homework7App.Equipment.Helmets
 {
-    class BronzeHelmet
+    public
+    class BronzeHelmet:Helmet
     {
+        public int defence { get; set; }
+        public BronzeHelmet()
+        {
+            name = "Bronze Helmet";
+            weight = 4;
+            price = 20;
+            rarity = 3;
+            defence = 3;
+        }
     }
 }
