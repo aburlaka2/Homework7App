87d834fdd85318443555606c0477ebd5a5b39efa
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:45:05 +0200

    Update Goblin.cs

--- Homework7App/Character/Race/Goblin.cs
+++ Homework7App/Character/Race/Goblin.cs
@@ -6,5 +6,14 @@ namespace Homework7App.Character.Race
 {
     class Goblin: Race, INotPlayble
     {
+        public Goblin()
+        {
+            CharacterRace = "Goblin";
+            RaceHealth = 6;
+            RaceMana = 6;
+            RaceStreghth = 5;
+            RaceAgilty = 8;
+            RaceIntelect = 4;
+        }
     }
 }
