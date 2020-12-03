6759b112455e4e251bfc47c173c15c8798c5384d
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:45:30 +0200

    Update CopperArmor.cs

--- Homework7App/Equipment/Armors/CopperArmor.cs
+++ Homework7App/Equipment/Armors/CopperArmor.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Armors
 {
-    class CopperArmor
+    class CopperArmor:Armor
     {
+        public int defence { get; set; }
+        public CopperArmor()
+        {
+            name = "Copper Armor";
+            weight = 10;
+            price = 40;
+            rarity = 2;
+            defence = 3;
+        }
     }
 }
