7a6cf27796c1cb345f36fd299c1295854bceb653
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:39 +0200

    Update Darkelf.cs

--- Homework7App/Character/Race/Darkelf.cs
+++ Homework7App/Character/Race/Darkelf.cs
@@ -6,5 +6,14 @@ namespace Homework7App.Character.Race
 {
     class Darkelf:Race,INotPlayble
     {
+        public Darkelf()
+        {
+            CharacterRace = "Darkelf";
+            RaceHealth = 7;
+            RaceMana = 10;
+            RaceStreghth = 7;
+            RaceAgilty = 8;
+            RaceIntelect = 6;
+        }
     }
 }
