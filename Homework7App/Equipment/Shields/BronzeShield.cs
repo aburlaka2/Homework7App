8d2fe377cab3962925eeb85a4d00ec9a155bf505
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:46:55 +0200

    Update BronzeShield.cs

--- Homework7App/Equipment/Shields/BronzeShield.cs
+++ Homework7App/Equipment/Shields/BronzeShield.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Shields
 {
-    class BronzeShield
+    class BronzeShield:Shield
     {
+        public int defence { get; set; }
+        public BronzeShield()
+        {
+            name = "Bronze Shield";
+            weight = 6;
+            price = 25;
+            rarity = 3;
+            defence = 3;
+        }
     }
 }
