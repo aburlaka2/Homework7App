accce9e743fd1872c5ad512b64c819d76a99707d
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:20 +0200

    Update Fighter.cs

--- Homework7App/Character/Profession/Fighter.cs
+++ Homework7App/Character/Profession/Fighter.cs
@@ -6,5 +6,14 @@ namespace Homework7App.Character.Profession
 {
     class Fighter: Profession
     {
+        public Fighter()
+        {
+            CharacterProfession = "Fighter";
+            ProfessionHealth = 5;
+            ProfessionMana = 1;
+            ProfessionStreghth = 7;
+            ProfessionAgilty = 3;
+            ProfessionIntelect = 1;
+        }
     }
 }
