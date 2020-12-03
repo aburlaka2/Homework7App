4ae313483b2cb53b7980f6c42bb6069013e988de
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:46:50 +0200

    Update SteelHelmet.cs

--- Homework7App/Equipment/Helmets/SteelHelmet.cs
+++ Homework7App/Equipment/Helmets/SteelHelmet.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Helmets
 {
-    class SteelHelmet
+    class SteelHelmet:Helmet
     {
+        public int defence { get; set; }
+        public SteelHelmet()
+        {
+            name = "Steel Helmet";
+            weight = 8;
+            price = 50;
+            rarity = 6;
+            defence = 5;
+        }
     }
 }
