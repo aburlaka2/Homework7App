785836ad43f657d1b45798cb3ab65a1e3eba5edd
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:47:22 +0200

    Update WoodenShield.cs

--- Homework7App/Equipment/Shields/WoodenShield.cs
+++ Homework7App/Equipment/Shields/WoodenShield.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Shields
 {
-    class WoodenShield
+    class WoodenShield:Shield
     {
+        public int defence { get; set; }
+        public WoodenShield()
+        {
+            name = "Wooden Shield";
+            weight = 3;
+            price = 10;
+            rarity = 1;
+            defence = 1;
+        }
     }
 }
