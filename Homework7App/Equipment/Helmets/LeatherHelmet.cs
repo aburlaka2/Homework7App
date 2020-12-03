616ccf42c94c4ef49eff65d060cfa1c7322048f6
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:46:40 +0200

    Create LeatherHelmet.cs

---
+++ Homework7App/Equipment/Helmets/LeatherHelmet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7App.Equipment.Helmets
+{
+    class LeatherHelmet:Helmet
+    {
+        public int defence { get; set; }
+        public LeatherHelmet()
+        {
+            name = "Lether Helmet";
+            weight = 2;
+            price = 5;
+            rarity = 1;
+            defence = 1;
+        }
+    }
+}
