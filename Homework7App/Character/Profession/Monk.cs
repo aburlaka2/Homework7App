818b8a018154b02c91ac42d19d02cb1da32cefce
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:44:28 +0200

    Update Monk.cs

--- Homework7App/Character/Profession/Monk.cs
+++ Homework7App/Character/Profession/Monk.cs
@@ -6,5 +6,15 @@ namespace Homework7App.Character.Profession
 {
     class Monk:Profession
     {
+        public Monk()
+        {
+            CharacterProfession = "Monk";
+            ProfessionHealth = 7;
+            ProfessionMana = 2;
+            ProfessionStreghth = 3;
+            ProfessionAgilty = 2;
+            ProfessionIntelect = 3;
+        }
     }
 }
+
