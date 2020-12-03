111184ead0eec12930a87edad19b9f2ed67bb159
aburlaka2 <aburlaka1@gmail.com>
2020-12-03 21:47:17 +0200

    Update SteelShield.cs

--- Homework7App/Equipment/Shields/SteelShield.cs
+++ Homework7App/Equipment/Shields/SteelShield.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Homework7App.Equipment.Shields
 {
-    class SteelShield
+    class SteelShield:Shield
     {
+        public int defence { get; set; }
+        public SteelShield()
+        {
+            name = "Steel Shield";
+            weight = 10;
+            price = 60;
+            rarity = 7;
+            defence = 7;
+        }
     }
 }
