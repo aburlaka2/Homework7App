9c136d0f413458080b2560d81b12690218d27f06
aburlaka2 <aburlaka1@gmail.com>
2020-12-06 15:47:11 +0200

    Update GameItem.cs

--- Homework7App/GameMarket/Market/Special/GameItem.cs
+++ Homework7App/GameMarket/Market/Special/GameItem.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Homework7App.GameMarket.Market.Special
+namespace Homework7App.Monetization.Market.Special
 {
-    class GameItem
+    public class GameItem : MarketItem
     {
     }
 }
