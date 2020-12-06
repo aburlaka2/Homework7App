63ae39aea0050e32f042b12f6e63de0f66956d7d
aburlaka2 <aburlaka1@gmail.com>
2020-12-06 15:44:39 +0200

    Create Account.cs

---
+++ Homework7App/GameMarket/Account.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Homework7App.GameMarket;
+using Homework7App;
+
+namespace Homework7App.GameMarket
+{
+    public class Account
+    {
+        public User Owner { get; set; }
+
+        public PaymentInfo PaymentInfo { get; set; }
+        public Transaction[] PaymentHistory { get; set; }
+
+        public Balance Balance { get; set; }
+
+        public MarketItem[] PurchasedItems { get; }
+    }
+}
